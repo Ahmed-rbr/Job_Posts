7c9ebf69b964a374e6d3310478834056039c1817
rabar <ahmedrbr01@gmail.com>
2025-03-28 22:09:00 +0000

    added post details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./Pages/JobsPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 import JobPage, { jobLoader } from "./Pages/JobPage";
-
+import Spinner from "./components/Spinner";
+import { Suspense } from "react";
+import AddJobPage from "./Pages/AddJobPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,20 +17,31 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
+        path: "jobs",
+        element: <JobsPage />,
+        errorElement: <NotFoundPage />,
+      },
+      {
         path: "jobs/:id",
         element: <JobPage />,
         loader: jobLoader,
         errorElement: <NotFoundPage />,
       },
-
-      { path: "*", element: <NotFoundPage /> },
-      { path: "/jobs/:id", element: <JobPage />, loader: { jobLoader } },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+      { path: "add-job", element: <AddJobPage /> },
     ],
   },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<Spinner />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 };
 
 export default App;
