4fa3310c5ebc5e8da19d7af78689401ab3aa4fb2
rabar <ahmedrbr01@gmail.com>
2025-03-29 03:02:14 +0000

    fixed bug prevent from debloy

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: import.meta.env.DEV
-      ? {
-          "/api": {
-            target: "http://localhost:8000",
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ""),
-          },
-        }
-      : undefined,
+    proxy:
+      mode === "development"
+        ? {
+            "/api": {
+              target: "http://localhost:8000",
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+          }
+        : undefined,
   },
   define: {
-    "process.env": process.env,
+    "process.env": {},
   },
-});
+}));
